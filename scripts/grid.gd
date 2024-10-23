4d01d7b633f30a67c9f6abab37405cfcfe5b8d0f
Pixlent <2beheima@gmail.com>
2024-10-23 08:13:13 +0200

    feat: add drag selection

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -1,8 +1,12 @@
 extends Node2D
+class_name Grid
 
 const TILE = preload("res://scenes/tile.tscn")
 const GRID_SPACING: int = 150
 const GRID_SIZE: int = 8
+
+var tiles: Dictionary = {}
+var selection: Array = []
 
 @onready var game = $".."
 
@@ -11,7 +15,10 @@ func _ready():
 	for x in GRID_SIZE:
 		for y in GRID_SIZE:
 			var tile = TILE.instantiate()
+			tile.grid = self
+			tile.pos = Vector2(x, y)
 			tile.position = Vector2((x-(GRID_SIZE/2)) * GRID_SPACING, (y-(GRID_SIZE/2)) * GRID_SPACING)
+			tiles.merge({Vector2(x, y):tile})
 			add_child(tile)
 	
 	pass # Replace with function body.
@@ -19,3 +26,40 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if Input.is_action_pressed("left_mouse_button"):
+		var tile: Tile = get_tile_at_mouse_position()
+		if !tile:
+			return
+		print(tile.position)
+		tile.select()
+
+func erase_tile(pos: Vector2):
+	var tile = tiles.get(pos)
+	
+	if !tile:
+		print("Tile doesn't exist: " + str(pos))
+		return
+	
+	#tile.queue_free()
+	#tiles.erase(pos)
+	
+	print("Tile selected: " + str(pos))
+
+func get_tile(pos: Vector2):
+	return tiles.get(pos)
+
+func get_tile_at_mouse_position() -> Tile:
+	var space = get_world_2d().direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = get_global_mouse_position()
+	query.collide_with_areas = true
+	var results = space.intersect_point(query)
+	if results.size() < 1:
+		return null
+	var tile = results[0]["collider"].get_parent()
+	
+	if is_instance_of(tile, Tile):
+		return tile
+	return null
--- scripts/tile.gd
+++ scripts/tile.gd
@@ -1,22 +1,26 @@
 extends Node2D
+class_name Tile
 
 @onready var label = $Label
+@onready var sprite_2d = $Sprite2D
+
+@export var grid: Grid
+@export var pos: Vector2
 
 var random := RandomNumberGenerator.new()
+var selected: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	label.text = String.chr(random.randi_range(65, 90))
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
 	
+	pass
 
-func _on_label_gui_input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_mouse_button"):
-		print("left_click: " + str(position))
-		queue_free()
-	pass # Replace with function body.
+func select():
+	if !selected:
+		selected = true
+		sprite_2d.show()
+		grid.erase_tile(pos)
